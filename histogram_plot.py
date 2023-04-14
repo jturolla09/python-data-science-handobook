e77ee8243a71b9ce39066adb938dcfe9ccd29330
jturolla09 <joaop.turolla@gmail.com>
2023-04-14 09:12:17 -0300

    Update histogram_plot.py

--- histogram_plot.py
+++ histogram_plot.py
@@ -2,13 +2,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 sns.set(rc={'figure.figsize':(16,9)})
-
 fig, ax = plt.subplots()
-sns.histplot(data=results, x="Foo", ax=ax).set_title("Foo")
+
+sns.histplot(data=results, x="foo", hue = 'reference_date', hue_order = [datetime.date(2023,4,13), datetime.date(2022,12,31)], ax=ax).set_title("foo")
 
 ax2 = ax.twinx()
-sns.histplot(data=results, cumulative = True, stat='percent', element = 'poly', fill=False, x="perf_cumulative", ax=ax2)
+sns.histplot(data=results, cumulative = True, stat='percent', element = 'poly', fill=False, x="foo", hue = 'reference_date', hue_order = [datetime.date(2023,4,13), datetime.date(2022,12,31)], ax=ax2)
 ax2.grid(False)
+ax2.get_legend().remove()
 
 plt.tight_layout()
 plt.show()
